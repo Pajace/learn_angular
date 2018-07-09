296217542f09b8d29807e75f9db6ecd7898da4a5
Pajace Chen <pajacechen@google.com>
2018-07-09 10:57:19 +0800

    implement: clear searched text when press ESC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,17 +11,27 @@ export class AppComponent {
   imgurl = '/assets/images/logo.png';
 
   searchTextCount = 0;
+  searchText = '';
 
   constructor() {
   }
 
   changeTitle(altKey: boolean) {
     if (altKey) {
-      this.title  = 'The Will Will Web';
+      this.title = 'The Will Will Web';
     }
   }
 
   countSearchText(searchText: string) {
     this.searchTextCount = searchText.length;
+    this.searchText = searchText;
+  }
+
+  clearSearchText(key: string) {
+    if (key === 'Escape') {
+      this.searchText = '';
+      this.searchTextCount = 0;
+      console.log('success.');
+    }
   }
 }
