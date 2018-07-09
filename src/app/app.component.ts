c142a9f6e3e13e5de6ab72c84550661ad2b21cc4
Pajace Chen <pajacechen@google.com>
2018-07-09 10:50:45 +0800

    implement display searched text count

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,19 @@ export class AppComponent {
   title = 'app';
   url = 'http://blog.miniasp.com/';
   imgurl = '/assets/images/logo.png';
+
+  searchTextCount = 0;
+
   constructor() {
   }
+
   changeTitle(altKey: boolean) {
     if (altKey) {
       this.title  = 'The Will Will Web';
     }
+  }
+
+  countSearchText(searchText: string) {
+    this.searchTextCount = searchText.length;
   }
 }
